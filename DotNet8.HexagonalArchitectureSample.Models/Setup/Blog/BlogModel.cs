b44127de6376ebec8bff17885786a74c8ee9e9c3
Linn Thit <lth1212001@gmail.com>
2024-05-30 02:05:38 +0630

    -

--- DotNet8.HexagonalArchitectureSample.Models/Setup/Blog/BlogModel.cs
+++ DotNet8.HexagonalArchitectureSample.Models/Setup/Blog/BlogModel.cs
@@ -1,13 +1,12 @@
-﻿namespace DotNet8.HexagonalArchitectureSample.Models.Setup.Blog
+﻿namespace DotNet8.HexagonalArchitectureSample.Models.Setup.Blog;
+
+public class BlogModel
 {
-    public class BlogModel
-    {
-        public long BlogId { get; set; }
+    public long BlogId { get; set; }
 
-        public string BlogTitle { get; set; } = null!;
+    public string BlogTitle { get; set; } = null!;
 
-        public string BlogAuthor { get; set; } = null!;
+    public string BlogAuthor { get; set; } = null!;
 
-        public string BlogContent { get; set; } = null!;
-    }
+    public string BlogContent { get; set; } = null!;
 }
