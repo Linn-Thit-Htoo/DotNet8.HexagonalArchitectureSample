b40265e7f4afbf5db67f83ccb19d0c0d2b6b0ab3
Linn Thit <lth1212001@gmail.com>
2024-05-30 02:05:25 +0630

    -

--- DotNet8.HexagonalArchitectureSample.Models/Setup/Blog/BlogModel.cs
+++ DotNet8.HexagonalArchitectureSample.Models/Setup/Blog/BlogModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.HexagonalArchitectureSample.Models.Setup.Blog
+﻿namespace DotNet8.HexagonalArchitectureSample.Models.Setup.Blog
 {
     public class BlogModel
     {
