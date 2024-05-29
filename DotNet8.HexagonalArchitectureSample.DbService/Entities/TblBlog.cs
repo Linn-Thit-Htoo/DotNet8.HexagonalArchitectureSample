b09f8dd9b31dd20c4123da151961218e2e9a88cb
Linn Thit <lth1212001@gmail.com>
2024-05-30 02:04:14 +0630

    -

--- DotNet8.HexagonalArchitectureSample.DbService/Entities/TblBlog.cs
+++ DotNet8.HexagonalArchitectureSample.DbService/Entities/TblBlog.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace DotNet8.HexagonalArchitectureSample.DbService.Entities;
+﻿namespace DotNet8.HexagonalArchitectureSample.DbService.Entities;
 
 public partial class TblBlog
 {
