22001021f28573b23a67851a1f5b91cdc8bdcbb2
Linn Thit <lth1212001@gmail.com>
2024-05-30 02:05:55 +0630

    -

--- DotNet8.HexagonalArchitectureSample.Models/Setup/Blog/BlogRequestModel.cs
+++ DotNet8.HexagonalArchitectureSample.Models/Setup/Blog/BlogRequestModel.cs
@@ -1,12 +1,11 @@
-﻿namespace DotNet8.HexagonalArchitectureSample.Models.Setup.Blog
+﻿namespace DotNet8.HexagonalArchitectureSample.Models.Setup.Blog;
+
+public class BlogRequestModel
 {
-    public class BlogRequestModel
-    {
 
-        public string BlogTitle { get; set; } = null!;
+    public string BlogTitle { get; set; } = null!;
 
-        public string BlogAuthor { get; set; } = null!;
+    public string BlogAuthor { get; set; } = null!;
 
-        public string BlogContent { get; set; } = null!;
-    }
+    public string BlogContent { get; set; } = null!;
 }
