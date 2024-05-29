37b7352847054036b63f944da8d62ee92315b256
Linn Thit <lth1212001@gmail.com>
2024-05-30 02:07:48 +0630

    -

--- DotNet8.HexagonalArchitectureSample/Features/BaseController.cs
+++ DotNet8.HexagonalArchitectureSample/Features/BaseController.cs
@@ -1,20 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
-namespace DotNet8.HexagonalArchitectureSample.Features
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController : ControllerBase
-    {
-        protected IActionResult Content(object obj)
-        {
-            return Content(JsonConvert.SerializeObject(obj), "application/json");
-        }
+namespace DotNet8.HexagonalArchitectureSample.Features;
 
-        protected IActionResult InternalServerError(Exception ex)
-        {
-            return StatusCode(500, ex.Message);
-        }
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
+{
+    protected IActionResult Content(object obj)
+    {
+        return Content(JsonConvert.SerializeObject(obj), "application/json");
+    }
+
+    protected IActionResult InternalServerError(Exception ex)
+    {
+        return StatusCode(500, ex.Message);
     }
 }
