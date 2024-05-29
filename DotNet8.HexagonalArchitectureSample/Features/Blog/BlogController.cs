47a83da25d27e91ed79a8531e856d8f733fbfc7b
Linn Thit <lth1212001@gmail.com>
2024-05-30 02:07:05 +0630

    -

--- DotNet8.HexagonalArchitectureSample/Features/Blog/BlogController.cs
+++ DotNet8.HexagonalArchitectureSample/Features/Blog/BlogController.cs
@@ -1,87 +1,86 @@
 ﻿using DotNet8.HexagonalArchitectureSample.Models.Setup.Blog;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNet8.HexagonalArchitectureSample.Features.Blog
+namespace DotNet8.HexagonalArchitectureSample.Features.Blog;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : BaseController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : BaseController
+    private readonly BL_Blog _bL_Blog;
+
+    public BlogController(BL_Blog bL_Blog)
     {
-        private readonly BL_Blog _bL_Blog;
+        _bL_Blog = bL_Blog;
+    }
 
-        public BlogController(BL_Blog bL_Blog)
+    [HttpGet]
+    public async Task<IActionResult> GetBlogs()
+    {
+        try
         {
-            _bL_Blog = bL_Blog;
+            var lst = await _bL_Blog.GetBlogsAsync();
+            return Content(lst);
         }
-
-        [HttpGet]
-        public async Task<IActionResult> GetBlogs()
+        catch (Exception ex)
         {
-            try
-            {
-                var lst = await _bL_Blog.GetBlogsAsync();
-                return Content(lst);
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            }
+            return InternalServerError(ex);
         }
+    }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetBlog(long id)
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetBlog(long id)
+    {
+        try
         {
-            try
-            {
-                var item = await _bL_Blog.GetBlogByIdAsync(id);
-                return Content(item);
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            }
+            var item = await _bL_Blog.GetBlogByIdAsync(id);
+            return Content(item);
         }
-
-        [HttpPost]
-        public async Task<IActionResult> CreateBlog([FromBody] BlogRequestModel requestModel)
+        catch (Exception ex)
         {
-            try
-            {
-                int result = await _bL_Blog.CreateBlogAsync(requestModel);
-                return result > 0 ? StatusCode(201, "Blog Created.") : BadRequest("Creating Fail.");
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            }
+            return InternalServerError(ex);
         }
+    }
 
-        [HttpPatch("{id}")]
-        public async Task<IActionResult> PatchBlog([FromBody] BlogRequestModel requestModel, long id)
+    [HttpPost]
+    public async Task<IActionResult> CreateBlog([FromBody] BlogRequestModel requestModel)
+    {
+        try
         {
-            try
-            {
-                int result = await _bL_Blog.PatchBlogAsync(requestModel, id);
-                return result > 0 ? StatusCode(201, "Blog Updated.") : BadRequest("Updating Fail.");
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            }
+            int result = await _bL_Blog.CreateBlogAsync(requestModel);
+            return result > 0 ? StatusCode(201, "Blog Created.") : BadRequest("Creating Fail.");
         }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteBLog(long id)
+        catch (Exception ex)
         {
-            try
-            {
-                int result = await _bL_Blog.DeleteBlogAsync(id);
-                return result > 0 ? StatusCode(201, "Blog Deleted.") : BadRequest("Deleting Fail.");
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            }
+            return InternalServerError(ex);
+        }
+    }
+
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> PatchBlog([FromBody] BlogRequestModel requestModel, long id)
+    {
+        try
+        {
+            int result = await _bL_Blog.PatchBlogAsync(requestModel, id);
+            return result > 0 ? StatusCode(201, "Blog Updated.") : BadRequest("Updating Fail.");
+        }
+        catch (Exception ex)
+        {
+            return InternalServerError(ex);
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteBLog(long id)
+    {
+        try
+        {
+            int result = await _bL_Blog.DeleteBlogAsync(id);
+            return result > 0 ? StatusCode(201, "Blog Deleted.") : BadRequest("Deleting Fail.");
+        }
+        catch (Exception ex)
+        {
+            return InternalServerError(ex);
         }
     }
 }
