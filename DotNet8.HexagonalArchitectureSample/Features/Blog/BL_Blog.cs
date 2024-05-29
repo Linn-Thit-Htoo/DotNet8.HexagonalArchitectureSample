dbd1378edbdb53e732553e28114fda9cf53e9137
Linn Thit <lth1212001@gmail.com>
2024-05-30 02:06:43 +0630

    -

--- DotNet8.HexagonalArchitectureSample/Features/Blog/BL_Blog.cs
+++ DotNet8.HexagonalArchitectureSample/Features/Blog/BL_Blog.cs
@@ -1,57 +1,56 @@
 ﻿using DotNet8.HexagonalArchitectureSample.Models.Setup.Blog;
 
-namespace DotNet8.HexagonalArchitectureSample.Features.Blog
+namespace DotNet8.HexagonalArchitectureSample.Features.Blog;
+
+public class BL_Blog
 {
-    public class BL_Blog
+    private readonly DA_Blog _dA_Blog;
+
+    public BL_Blog(DA_Blog dA_Blog)
     {
-        private readonly DA_Blog _dA_Blog;
+        _dA_Blog = dA_Blog;
+    }
 
-        public BL_Blog(DA_Blog dA_Blog)
-        {
-            _dA_Blog = dA_Blog;
-        }
+    public async Task<BlogListResponseModel> GetBlogsAsync()
+    {
+        return await _dA_Blog.GetBlogsAsync();
+    }
 
-        public async Task<BlogListResponseModel> GetBlogsAsync()
-        {
-            return await _dA_Blog.GetBlogsAsync();
-        }
+    public async Task<BlogModel> GetBlogByIdAsync(long id)
+    {
+        if (id <= 0)
+            throw new Exception("Id is invalid.");
 
-        public async Task<BlogModel> GetBlogByIdAsync(long id)
-        {
-            if (id <= 0)
-                throw new Exception("Id is invalid.");
+        return await _dA_Blog.GetBlogByIdAsync(id);
+    }
 
-            return await _dA_Blog.GetBlogByIdAsync(id);
-        }
+    public async Task<int> CreateBlogAsync(BlogRequestModel requestModel)
+    {
+        if (string.IsNullOrEmpty(requestModel.BlogTitle))
+            throw new Exception("Blog Title cannot be empty.");
 
-        public async Task<int> CreateBlogAsync(BlogRequestModel requestModel)
-        {
-            if (string.IsNullOrEmpty(requestModel.BlogTitle))
-                throw new Exception("Blog Title cannot be empty.");
+        if (string.IsNullOrEmpty(requestModel.BlogAuthor))
+            throw new Exception("Blog Author cannot be empty.");
 
-            if (string.IsNullOrEmpty(requestModel.BlogAuthor))
-                throw new Exception("Blog Author cannot be empty.");
+        if (string.IsNullOrEmpty(requestModel.BlogContent))
+            throw new Exception("Blog Content cannot be empty.");
 
-            if (string.IsNullOrEmpty(requestModel.BlogContent))
-                throw new Exception("Blog Content cannot be empty.");
+        return await _dA_Blog.CreateBlogAsync(requestModel);
+    }
 
-            return await _dA_Blog.CreateBlogAsync(requestModel);
-        }
+    public async Task<int> PatchBlogAsync(BlogRequestModel requestModel, long id)
+    {
+        if (id <= 0)
+            throw new Exception("Id is invalid.");
 
-        public async Task<int> PatchBlogAsync(BlogRequestModel requestModel, long id)
-        {
-            if (id <= 0)
-                throw new Exception("Id is invalid.");
+        return await _dA_Blog.PatchBlogAsync(requestModel, id);
+    }
 
-            return await _dA_Blog.PatchBlogAsync(requestModel, id);
-        }
+    public async Task<int> DeleteBlogAsync(long id)
+    {
+        if (id <= 0)
+            throw new Exception("Id is invalid.");
 
-        public async Task<int> DeleteBlogAsync(long id)
-        {
-            if (id <= 0)
-                throw new Exception("Id is invalid.");
-
-            return await _dA_Blog.DeleteBlogAsync(id);
-        }
+        return await _dA_Blog.DeleteBlogAsync(id);
     }
 }
