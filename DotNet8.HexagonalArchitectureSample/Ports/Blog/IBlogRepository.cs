bac0ed8c00a4bfdb264c20102f4482267e32a223
Linn Thit <lth1212001@gmail.com>
2024-05-30 02:08:04 +0630

    -

--- DotNet8.HexagonalArchitectureSample/Ports/Blog/IBlogRepository.cs
+++ DotNet8.HexagonalArchitectureSample/Ports/Blog/IBlogRepository.cs
@@ -1,13 +1,12 @@
 ﻿using DotNet8.HexagonalArchitectureSample.Models.Setup.Blog;
 
-namespace DotNet8.HexagonalArchitectureSample.Ports.Blog
+namespace DotNet8.HexagonalArchitectureSample.Ports.Blog;
+
+public interface IBlogRepository
 {
-    public interface IBlogRepository
-    {
-        Task<BlogListResponseModel> GetBlogsListAsync();
-        Task<BlogModel> GetBlogByIdAsync(long id);
-        Task<int> CreateBlogAsync(BlogRequestModel requestModel);
-        Task<int> PatchBlogAsync(BlogRequestModel requestModel, long id);
-        Task<int> DeleteBlogAsync(long id);
-    }
+    Task<BlogListResponseModel> GetBlogsListAsync();
+    Task<BlogModel> GetBlogByIdAsync(long id);
+    Task<int> CreateBlogAsync(BlogRequestModel requestModel);
+    Task<int> PatchBlogAsync(BlogRequestModel requestModel, long id);
+    Task<int> DeleteBlogAsync(long id);
 }
