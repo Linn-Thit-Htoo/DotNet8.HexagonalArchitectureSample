a5690b36133a680ea02cdd2f44cf3ca322bf59e1
Linn Thit <lth1212001@gmail.com>
2024-05-30 02:08:13 +0630

    -

--- DotNet8.HexagonalArchitectureSample/ModularService.cs
+++ DotNet8.HexagonalArchitectureSample/ModularService.cs
@@ -4,54 +4,53 @@ using DotNet8.HexagonalArchitectureSample.Features.Blog;
 using DotNet8.HexagonalArchitectureSample.Ports.Blog;
 using Microsoft.EntityFrameworkCore;
 
-namespace DotNet8.HexagonalArchitectureSample
+namespace DotNet8.HexagonalArchitectureSample;
+
+public static class ModularService
 {
-    public static class ModularService
+    public static IServiceCollection AddServices(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddServices(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            services.AddDbContextService(builder)
-                .AddDataAccessServices()
-                .AddBusinessLogicServices()
-                .AddRepositoriesService()
-                .AddJsonServices(builder);
-            return services;
-        }
+        services.AddDbContextService(builder)
+            .AddDataAccessServices()
+            .AddBusinessLogicServices()
+            .AddRepositoriesService()
+            .AddJsonServices(builder);
+        return services;
+    }
 
-        public static IServiceCollection AddBusinessLogicServices(this IServiceCollection services)
-        {
-            services.AddScoped<BL_Blog>();
-            return services;
-        }
+    public static IServiceCollection AddBusinessLogicServices(this IServiceCollection services)
+    {
+        services.AddScoped<BL_Blog>();
+        return services;
+    }
 
-        public static IServiceCollection AddDataAccessServices(this IServiceCollection services)
-        {
-            services.AddScoped<DA_Blog>();
-            return services;
-        }
+    public static IServiceCollection AddDataAccessServices(this IServiceCollection services)
+    {
+        services.AddScoped<DA_Blog>();
+        return services;
+    }
 
-        public static IServiceCollection AddJsonServices(this IServiceCollection services, WebApplicationBuilder builder)
+    public static IServiceCollection AddJsonServices(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers().AddJsonOptions(opt =>
         {
-            builder.Services.AddControllers().AddJsonOptions(opt =>
-            {
-                opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-            });
-            return services;
-        }
+            opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+        });
+        return services;
+    }
 
-        public static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+    public static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        services.AddDbContext<AppDbContext>(opt =>
         {
-            services.AddDbContext<AppDbContext>(opt =>
-            {
-                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-            });
-            return services;
-        }
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+        });
+        return services;
+    }
 
-        public static IServiceCollection AddRepositoriesService(this IServiceCollection services)
-        {
-            services.AddScoped<IBlogRepository, BlogRepository>();
-            return services;
-        }
+    public static IServiceCollection AddRepositoriesService(this IServiceCollection services)
+    {
+        services.AddScoped<IBlogRepository, BlogRepository>();
+        return services;
     }
 }
