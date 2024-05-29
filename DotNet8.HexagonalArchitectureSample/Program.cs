c036b09d77cbc7fafeedae8b248833fc56e828be
Linn Thit <lth1212001@gmail.com>
2024-05-30 02:08:34 +0630

    -

--- DotNet8.HexagonalArchitectureSample/Program.cs
+++ DotNet8.HexagonalArchitectureSample/Program.cs
@@ -2,10 +2,7 @@ using DotNet8.HexagonalArchitectureSample;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -13,7 +10,6 @@ builder.Services.AddServices(builder);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
